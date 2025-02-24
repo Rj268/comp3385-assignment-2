bbfdb0bba7c78ba6dd2ebe7e1b485a85104ebe70
Ronnie Samuel <samuelronnie55@gmail.com>
2025-02-24 12:49:59 -0400

    Added sucess.blade.php

---
+++ resources/views/feedback/success.blade.php
@@ -0,0 +1,12 @@
+@extends("layouts.app")
+
+@section('content')
+
+<div class="container mt-5 text center">
+    <h2>Success!</h2>
+    <p>Thank you for your feedback. We will review your feedback and get back to you.</p>
+
+
+</div>
+
+@endsection
