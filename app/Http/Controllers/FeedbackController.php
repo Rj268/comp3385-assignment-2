ff9b95795164d20edf65fd7b7205145727899a7b
Ronnie Samuel <samuelronnie55@gmail.com>
2025-02-24 12:43:24 -0400

    Edited FeedbackController.php

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Mail\Feedback;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
 
 
 class FeedbackController extends Controller
@@ -13,16 +15,19 @@ class FeedbackController extends Controller
         return view('feedback-form');
     }
 
-    public function send(Request $request){
+    public function send(Request $request):RedirectResponse{
         $request->validate([
-            'fullname' => 'required|string',
+            'name' => 'required|string',
             'email' => 'required|email',
-            'message' => 'required|string'
+            'comment' => 'required'
         ]);
 
-        $fullname = $request->input('fullname');
-        $email = $request->input('email');
-        $message = $request->input('comment');
+        Log::info('Feedback received from ' . $request->name . ' (' . $request->email . '): ' . $request->comment);
 
-        Mail::to('comp3385@uwi.edu', 'COMP3385 Course Admin')->send(new Feedback($fullname, $email, $message));
+
+        Mail::to('comp3385@uwi.edu', 'COMP3385 Course Admin')->send(new Feedback($request->name, $request->email, $request->comment));
+
+        return redirect('/feedback/success')->with('Success', 'Feedback sent successfully!');
+
+
 }}
