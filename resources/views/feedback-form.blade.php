ec56eb8772fea98bdb2305f8e6e5a08240a5e8b9
Ronnie Samuel <samuelronnie55@gmail.com>
2025-02-24 12:53:05 -0400

    Edited feedback-form.blade.php

--- resources/views/feedback-form.blade.php
+++ resources/views/feedback-form.blade.php
@@ -1,30 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>Feedback Form</h2>
 
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
+<div class="container mt-5">
+    <h2 class="mb-4">Feedback Form</h2>
+    <form action="/feedback/send" method="POST" class="card p-4 shadow">
+        @csrf
+        <div class="mb-3">
+            <label for="fullname">Full Name <span class="text-danger">(Required)</span></label>
+            <input type="text" name="name" class="form-control w-100" required>
+        </div>
+        
+        <div class="mb-3">
+            <label for="email">Email <span class="text-danger">(Required)</span></label>
+            <input type="email"  name="email" class="form-control w-100" required>
+        </div>
+        
+        <div>
+            <label for="comment">Comments <span class="text-danger">(Required)</span></label>
+            <textarea name="comment" class="form-control w-100" rows="4" required></textarea>
+            <p class="text-muted">Let us know what you think of our website</p>
+        </div>
+        
+        <button type="submit" class="btn btn-primary btn-lg w-auto">Submit</button>
+    </form>
 </div>
-@endif
-
-<form action="/feedback/send" method="POST">
-    @csrf
-    <label for="fullname">Full Name:</label>
-    <input type="text" id="fullname" name="fullname" required>
-
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required>
-
-    <label for="comment">Commenst:</label>
-    <textarea name="comment" id="comment" required></textarea>
-    <p>Let us know what you think of our website</p>
-
-    <button type="submit">Submit</button>
-</form>
 @endsection
